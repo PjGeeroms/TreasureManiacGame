130fc6985499c05dcc093fca278131f265ccacdc
Robin <Robin@Robin-PC>
2014-04-23 22:34:55 +0200

    Correcte javabestanden iteratie2+Toevoegen MainPanel, DetailHero, HeroPanel, NumberField en doorvoeren van de nodige wijzigen aan de andere klassen + Avatar choicebox

--- src/domein/DomeinController.java
+++ src/domein/DomeinController.java
@@ -19,7 +19,12 @@ public class DomeinController {
     private final TreasureMapper treasureMapper;
 
     /**
-     * Constructor for DomeinController, initializes both Mappers
+     * Constructor for DomeinController, initializes monsterMapper and
+     * treasureMapper
+     *
+     * @see persistentie.MonsterMapper
+     * @see persistentie.TreasureMapper
+     *
      */
     public DomeinController() {
         monsterMapper = new MonsterMapper();
@@ -27,8 +32,10 @@ public class DomeinController {
     }
 
     /**
+     * Add a treasure to the database
      *
-     * @param treasure the treasure to be added by using treasureMapper
+     * @param treasure the treasure to be added by using {@link persistentie.TreasureMapper#addTreasure(Treasure) addTreasure-method
+     * } from treasureMapper
      * @return true if treasure was added succesfully
      */
     public boolean addTreasure(Treasure treasure) {
@@ -36,8 +43,10 @@ public class DomeinController {
     }
 
     /**
+     * Add a monster to the database
      *
-     * @param monster the monster to be added by using monsterMapper
+     * @param monster the monster to be added by using {@link persistentie.MonsterMapper#addMonster(Monster) addMonster-method
+     * } from monsterMapper
      * @return true if Monster was added succesfully
      */
     public boolean addMonster(Monster monster) {
@@ -45,8 +54,11 @@ public class DomeinController {
     }
 
     /**
+     * Delete a treasure in the database
      *
-     * @param treasure the treasture to be deleted by using treasureMapper
+     * @param treasure the treasture to be deleted by using
+     * {@link persistentie.TreasureMapper#deleteTreasure(Treasure) deleteTreasure-method}
+     * from treasureMapper
      * @return true if treasure was deleted succesfully
      */
     public boolean deleteTreasure(Treasure treasure) {
@@ -54,8 +66,11 @@ public class DomeinController {
     }
 
     /**
+     * Delete a treasure in the database
      *
-     * @param id the id of the treasure to be deleted by using treasureMapper
+     * @param id the id of the treasure to be deleted by using
+     * {@link persistentie.TreasureMapper#deleteTreasure(int) deleteTreasure-method}
+     * from treasureMapper
      * @return true if treasure was deleted succesfully
      */
     public boolean deleteTreasure(int id) {
@@ -63,8 +78,10 @@ public class DomeinController {
     }
 
     /**
+     * Delete a monster in the database
      *
-     * @param monster the monster to be deleted by using monsterMapper
+     * @param monster the monster to be deleted by using {@link persistentie.MonsterMapper#deleteMonster(Monster) deleteMonster-method
+     * } from monsterMapper
      * @return true if monster was deleted succesfully
      */
     public boolean deleteMonster(Monster monster) {
@@ -72,8 +89,10 @@ public class DomeinController {
     }
 
     /**
+     * Delete a monster in the database
      *
-     * @param id the id of the monster to be deleted by using monsterMapper
+     * @param id the id of the monster to be deleted by using {@link persistentie.MonsterMapper#deleteMonster(int) deleteMonster-method
+     * } from monsterMapper
      * @return true if monster was deleted succesfully
      */
     public boolean deleteMonster(int id) {
@@ -81,8 +100,10 @@ public class DomeinController {
     }
 
     /**
+     * Update a monster in the database
      *
-     * @param monster the monster to be updated by using monsterMapper
+     * @param monster the monster to be updated by using {@link persistentie.MonsterMapper#updateMonster(Monster) updateMonster-method
+     * } from monsterMapper
      * @return true if monster was succesfully updated
      */
     public boolean updateMonster(Monster monster) {
@@ -90,8 +111,11 @@ public class DomeinController {
     }
 
     /**
+     * Update a treasure in the database
      *
-     * @param treasure the treasure to be updated by using treasureMapper
+     * @param treasure the treasure to be updated by using
+     * {@link persistentie.TreasureMapper#updateTreasure(Treasure) updateTreasure-method}
+     * from treasureMapper
      * @return true if treasure was succesfully updated
      */
     public boolean updateTreasure(Treasure treasure) {
@@ -99,65 +123,83 @@ public class DomeinController {
     }
 
     /**
+     * Give a list of all monsters in the database
      *
-     * @return a list with all Monsters in database by using monsterMapper
+     * @return a list with all Monsters in database by using {@link persistentie.MonsterMapper#searchAllMonsters() searchAllMonsters-method
+     * } from monsterMapper
      */
     public List<Monster> searchAllMonsters() {
         return monsterMapper.searchAllMonsters();
     }
 
     /**
+     * Give a list of all treasures in the database
      *
-     * @return a list with all Treasures in database by using treasureMapper
+     * @return a list with all Treasures in database by using
+     * {@link persistentie.TreasureMapper#searchAllTreasures() searchAllTreasures()}
+     * from treasureMapper
      */
     public List<Treasure> searchAllTreasures() {
         return treasureMapper.searchAllTreasures();
     }
 
     /**
+     * Add a treasure to a monster using {@link persistentie.MonsterMapper#addTreasureToMonster(Monster,Treasure) addTreasureToMonster-method
+     * } from monsterMapper
      *
-     * @param monster
-     * @param treasure
-     * @return
+     * @param monster the monster to which we'll try to link a treasure
+     * @param treasure the treasure that will be linked to the monster
+     * @return true if treasure was succesfully linked to the monster, false if
+     * not
      */
     public boolean addTreasureToMonster(Monster monster, Treasure treasure) {
         return monsterMapper.addTreasureToMonster(monster, treasure);
     }
 
     /**
+     * Remove a treasure from a monster using {@link persistentie.MonsterMapper#removeTreasureFromMonster(Monster,Treasure) removeTreasureFromMonster-method
+     * } from monsterMapper
      *
-     * @param monster
-     * @param treasure
-     * @return
+     * @param monster the monster from which we'll try to remove a treasure
+     * @param treasure the treasure that will be removed from the monster
+     * @return true if treasure was succesfully removed from the monster, false
+     * if not
      */
     public boolean removeTreasureFromMonster(Monster monster, Treasure treasure) {
         return monsterMapper.removeTreasureFromMonster(monster, treasure);
     }
 
     /**
+     * Give a list of all treasures connected to a monster
      *
      * @param id the id of the monster you want to get the treasures from
      * @return a list of all the treasures a monster guards by using
-     * monsterMapper
+     * {@link persistentie.MonsterMapper#searchAllTreasuresFromMonster(int) removeTreasureFromMonster-method }
+     * from monsterMapper
      */
     public List<Treasure> searchAllTreasuresFromMonster(int id) {
         return monsterMapper.searchAllTreasuresFromMonster(id);
     }
 
     /**
+     * Give a list of all treasures connected to a monster
      *
      * @param monster the monster you want to get the treasures from
      * @return a list of all the treasures a monster guards by using
-     * monsterMapper
+     * {@link persistentie.MonsterMapper#searchAllTreasuresFromMonster(Monster) searchAllTreasuresFromMonster-method }
+     * from monsterMapper
      */
-    public List<Treasure> searchAllTreasureFromMonster(Monster monster) {
+    public List<Treasure> searchAllTreasuresFromMonster(Monster monster) {
         return monsterMapper.searchAllTreasuresFromMonster(monster);
     }
 
     /**
+     * Check connection of treasure with monster(s)
      *
-     * @param id the id of the treasure you want to check if it's linked with a
-     * monster using isUnconnected-method from TreasureMapper
+     * @param id the id of the treasure of which you want to check if it's
+     * linked with a monster using
+     * {@link persistentie.TreasureMapper#isUnconnected(int) isUnconnected-method}
+     * from TreasureMapper
      * @return 1 if unconnected, 0 otherwise or -1 in case an unexpected error
      * occurred
      *
@@ -166,11 +208,86 @@ public class DomeinController {
         return treasureMapper.isUnconnected(id);
     }
 
-    public Boolean addTreasureToMonster(int monsterId, int treasureId) {
+    /**
+     * Check connection of monster with treasure(s)
+     *
+     * @param id the id of the monster of which you want to check if is still
+     * has treasures linked to it using the {@link persistentie.MonsterMapper#isUnconnected(int) isUnconnected-method
+     * } from monsterMapper
+     * @return 1 if unconnected, 0 otherwise or -1 in case an unexpected error
+     * occurred
+     *
+     */
+    public int isUnconnectedMonster(int id) {
+        return monsterMapper.isUnconnected(id);
+    }
+
+    /**
+     * Add a treasure to a monster by using {@link persistentie.MonsterMapper#addTreasureToMonster(int,int) addTreasureToMonster-method
+     * } from monsterMapper
+     *
+     * @param monsterId the id of the monster that will be added a treasure to
+     * @param treasureId the id of the treasure that will be added to the
+     * monster
+     *
+     * @return true if linking was succesfull, false if not
+     *
+     */
+    public boolean addTreasureToMonster(int monsterId, int treasureId) {
         return monsterMapper.addTreasureToMonster(monsterId, treasureId);
     }
 
-    public Boolean removeTreasureFromMonster(int monsterId, int treasureId) {
+    /**
+     * Remove a treasure from a monster by using {@link persistentie.MonsterMapper#removeTreasureFromMonster(int,int) removeTreasureFromMonster-method
+     * } from monsterMapper
+     *
+     * @param monsterId the id of the monster of which a treasure will be
+     * removed from
+     * @param treasureId the id of the treasure that will be removed from the
+     * monster
+     *
+     * @return true if the link was succesfully broken, false if not
+     *
+     */
+    public boolean removeTreasureFromMonster(int monsterId, int treasureId) {
         return monsterMapper.removeTreasureFromMonster(monsterId, treasureId);
+    }
+
+    /**
+     * Check if treasure already exists in the database
+     *
+     * @param treasure the treasure whose presence in the database has to be
+     * checked
+     * @return true is treasure doesn't yet exist in the database, false if it
+     * does
+     */
+    public boolean isNewTreasure(Treasure treasure) {
+        List<Treasure> treasures = treasureMapper.searchAllTreasures();
+
+        for (Treasure t : treasures) {
+            if (t.equals(treasure)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Check if monster already exists in the database
+     *
+     * @param monster the monster whose presence in the database has to be
+     * checked
+     * @return true is monster doesn't yet exist in the database, false if it
+     * does
+     */
+    public boolean isNewMonster(Monster monster) {
+        List<Monster> monsters = monsterMapper.searchAllMonsters();
+
+        for (Monster m : monsters) {
+            if (m.equals(monster)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
